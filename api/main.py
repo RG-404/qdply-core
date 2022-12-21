7a77197a5a4ad65e3a8c7119f4c7c47acc231287
RG-404 <55778353+RG-404@users.noreply.github.com>
2022-12-22 05:14:26 +0530

    styled homepage

--- api/main.py
+++ api/main.py
@@ -55,7 +55,7 @@ def upload_file():
 
         return render_template('uploaded.html', data=data)
 
-    serverNameList = getHostedServerNames("/etc/nginx/sites-available/qdply")
+    serverNameList = getHostedServerNames("/Users/rishi/projects/qdply-core/nginx-config/qdply")
     return render_template('index.html', data=serverNameList)
 
 
